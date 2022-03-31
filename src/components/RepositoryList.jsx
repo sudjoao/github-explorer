44aee49bfde6c51331d5270c0eb917eb8937b15a
sudjoao <isudjoao@gmail.com>
2022-03-31 19:10:05 -0300

    Add and configure typescript in application

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,24 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
 import "../styles/repositories.scss";
 
-const repository = {
-    name: "sudjoao",
-    description: "Olá mundo",
-    link: "https://github.com/sudjoao/sudjoao"
-}
+
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/sudjoao/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, []);
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map((repository, index) => (
+                    <RepositoryItem repository={repository} key={index}/>
+                ))}
             </ul>
         </section>
     );
